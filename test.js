13158cd66220437336ca6065454d058ec38debb2
Kirill Karpin <snowfrost@mail.ru>
2020-04-21 19:26:17 +0500

    Fix async code to sync

--- test.js
+++ test.js
@@ -1,5 +1,26 @@
-testQuery((data) => {
-  testResult(data);
+let data = new Promise(function (resolve, reject) {
+  testQuery((data) => {
+    if (data) {
+      resolve(data);
+    }
+    else {
+      reject(new Error('empty data'));
+    }
+  });
+});
+const result = {
+  success: true,
+  data: null,
+  error: null,
+};
+
+data.then((data) => {
+  result.data = data;
+  testResult(result);
+}).catch((err) => {
+  result.success = false;
+  result.error = err;
+  testResult(result);
 });
 
 
